5511f8663a05a9d89cd392c51ec65bb09767feb4
Inigo-santiu <dstavroff@gmail.com>
2020-08-03 23:23:23 +0200

    final

---
+++ README.md
@@ -0,0 +1,3 @@
+This was an exercise for the Hacking Project that was done with help, contribution and correction by David, Daniel, Juan, and Kevin!
+
+Many thanks and appreciation to all of you.
---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,18 @@
+
+class WelcomeController < ApplicationController
+  def index
+  end
+  
+  def welcome_message
+    puts "#{params}"
+    puts "$" * 60 #ça affiche une ligne de 60 symboles $ facilement visible dans le terminal
+    puts params #tu sais que params doit s'afficher entre les 2
+    puts "$" * 60
+    @first_name = params[:first_name]
+    puts "User: #{@first_name}"
+    @gossip_count = Gossip.all.count
+    puts "count #{@gossip_count}"
+  end
+
+
+end
